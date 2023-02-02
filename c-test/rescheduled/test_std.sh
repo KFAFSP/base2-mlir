572c47294557368f29be22f392839334a11a3c53
HahaLan97 <jibi912c@gmail.com>
2023-02-02 11:44:44 +0100

    Remove c-test folder from remote repo.

--- c-test/rescheduled/test_std.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p temp && cd temp
-~/Desktop/base2-mlir/build/bin/base2-opt ../rescheduled.mlir -lower-affine -convert-scf-to-cf -convert-cf-to-llvm -convert-arith-to-llvm -convert-func-to-llvm -convert-memref-to-llvm -reconcile-unrealized-casts --llvm-legalize-for-export -o test_std.mlir
-~/Desktop/llvm/bin/mlir-translate test_std.mlir -mlir-to-llvmir -o test_std.ll
-~/Desktop/llvm/bin/llc test_std.ll -filetype=obj -o test_std.o
