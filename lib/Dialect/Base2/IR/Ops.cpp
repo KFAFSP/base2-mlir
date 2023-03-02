23637ce3f77ec2ff1fb7b767f9a174e1bbad1043
HahaLan97 <jiahong.bi@gmail.com>
2023-03-02 23:32:44 +0100

    Add "base2.fixed.add" op.

--- lib/Dialect/Base2/IR/Ops.cpp
+++ lib/Dialect/Base2/IR/Ops.cpp
@@ -7,6 +7,7 @@
 #include "base2-mlir/Dialect/Base2/IR/Ops.h"
 
 #include "base2-mlir/Dialect/Base2/Analysis/DynamicValue.h"
+#include "base2-mlir/Dialect/Base2/Analysis/FixedPointInterpreter.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/IR/TypeUtilities.h"
@@ -342,6 +343,55 @@ OpFoldResult ModOp::fold(ModOp::FoldAdaptor adaptor)
         [&](auto &&lhs, auto &&rhs) { return lhs.mod(rhs); },
         getOperands(),
         adaptor.getOperands());
+}
+
+//===----------------------------------------------------------------------===//
+// Fixed-point operations
+//===----------------------------------------------------------------------===//
+
+template<class Fn>
+[[nodiscard]] static LogicalResult inferFixedPointType(
+    ValueRange operands,
+    SmallVectorImpl<Type> &resultTypes,
+    Fn fn)
+{
+    assert(operands.size() == 2);
+
+    // Get the underlying FixedPointLikeType.
+    const auto lhsTy =
+        getElementTypeOrSelf(operands.front()).dyn_cast<FixedPointLikeType>();
+    const auto rhsTy =
+        getElementTypeOrSelf(operands.back()).dyn_cast<FixedPointLikeType>();
+    if (!lhsTy || !rhsTy) return failure();
+
+    // Get the FixedPointSemantics.
+    const auto lhsSema = lhsTy.getSemantics();
+    const auto rhsSema = rhsTy.getSemantics();
+
+    // Infer the result semantics and return the canonical type.
+    const auto outSema = fn(lhsSema, rhsSema);
+    resultTypes.push_back(getSameShape(
+        lhsTy,
+        FixedPointType::get(
+            outSema.getIntegerType(),
+            outSema.getFractionalBits())));
+    return success();
+}
+
+LogicalResult FixedAddOp::inferReturnTypes(
+    MLIRContext*,
+    Optional<Location>,
+    ValueRange operands,
+    DictionaryAttr,
+    RegionRange,
+    SmallVectorImpl<Type> &resultTypes)
+{
+    return inferFixedPointType(
+        operands,
+        resultTypes,
+        [](auto &&lhs, auto &&rhs) {
+            return FixedPointInterpreter::add(lhs, rhs);
+        });
 }
 
 //===----------------------------------------------------------------------===//
